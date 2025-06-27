206f76004d0e92511843bed155b51e5ee1d7af07
mihaipogoane <mihaipogoane@outlook.com>
2025-06-27 12:47:23 +0300

    Update noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -12,7 +12,7 @@ from typing import List, Optional
 with SB(uc=True, test=True) as sb:
 
     if True:
-        url = "https://kick.com/vortex-ktv"
+        url = "https://kick.com/brutalles"
         sb.uc_open_with_reconnect(url, 5)
         sb.uc_gui_click_captcha()
         sb.sleep(1)
